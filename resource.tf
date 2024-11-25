42299bc52430edf9fb63a5a56f767ab86a9810f1
Shubham-Bawa <51166582+Shubham-Bawa@users.noreply.github.com>
2024-11-25 09:37:29 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -36,3 +36,15 @@ resource "aws_dynamodb_table" "locks" {
    type = "S"
  }
 }
+
+
+output "s3_bucket_arn" {
+  value       = aws_s3_bucket.bckt.arn
+  description = "arn of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.locks.name
+  description = "The name of the DynamoDB table"
+  
+}
