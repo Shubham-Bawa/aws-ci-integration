9811d286065ed3e5a3c16ca2f1de6285780a1e35
Shubham-Bawa <51166582+Shubham-Bawa@users.noreply.github.com>
2024-11-25 12:31:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-terraform {
- backend "s3" {
- bucket = "shubhamtest1024"
- key = "globalstate/s3/terraform.tfstate"
- region = "eu-west-1"
- # Replace this with your DynamoDB table name!
- dynamodb_table = "Neeha-902903July-dblocks"
- encrypt = true
- }
-}
 
 
 provider "aws" {
